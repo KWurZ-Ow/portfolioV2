15fc1ab97db807bb5fe31d349a32ef561e0c9718
Marius Proton <82665675+KWurZ-Ow@users.noreply.github.com>
2022-01-29 21:42:30 +0100

    loading and landing page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/index.css";
+import "./css/App.scss";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+import Home from "./pages/Home";
+import Error404 from "./pages/Error404";
+import Creation from "./pages/Creation";
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: "#00e2ff",
+    },
+  },
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ThemeProvider theme={theme}>
+    <React.StrictMode>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/creation/:questionNumber" element={<Creation />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </Router>
+    </React.StrictMode>
+  </ThemeProvider>,
+  document.getElementById("root")
 );
